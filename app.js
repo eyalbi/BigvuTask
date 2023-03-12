5aa2c109778de7d509f60bba319bb2bf36259f03
Eyal Bismuth <eyalbi24@gmail.com>
2023-03-12 16:15:53 +0200

    update multiple args run

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const {handler} = require('./handler');
 const args = process.argv;
-const res = handler({
-  "body": `{\"url\":\"${args[2]}\"}`
-})
+//add Default arg
+if (args.length < 3){
+  args.push('google.com');
+}
 
-res.then((res)=>{
-  console.log(res);
-})
+//handler execution function
+const runHandler = (url) => {
+  const res = handler({
+    "body": `{\"url\":\"${url}\"}`
+  })
+  
+  res.then((res)=>{
+    console.log(res);
+  })
+};
+
+const runApplication = (urlArray) =>{
+  const promises = [];
+  //Create Promise for every url and Push to Promises array
+  for (let i = 2; i < urlArray.length; i++) {
+    promises.push(new Promise((resolve, reject) => {
+      runHandler(args[i])
+    }))
+  }
+
+  //run all url async
+  Promise.all(promises)
+  .then((messages) => {
+    console.log(messages);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+};
+
+runApplication(args);
